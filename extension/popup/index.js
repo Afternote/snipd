02180723f1aafa7472fcc3352c5bb7f8687849cf
Alaap Surendran <alaapsurendran@gmail.com>
2023-03-04 11:40:25 +0530

    First proof of concept les goooo

--- extension/popup/index.js
+++ extension/popup/index.js
@@ -1,1 +1,14 @@
-document.body.innerHTML = "lmao it worked";
+(async () => {
+    const [tab] = await browser.tabs.query({active: true, currentWindow: true});
+    let result;
+    try {
+    [{result}] = await browser.scripting.executeScript({
+        target: {tabId: tab.id},
+        func: () => window.getSelection().toString(),
+        });
+    } catch (e) {
+        document.body.append('error\n');
+    }
+    document.body.append('Selection: ' + result);
+    document.body.append((new Date()).toString());
+})();
