603be2a8239c8d4541c2fc0362bd728f50b1271e
Alaap Surendran <alaapsurendran@gmail.com>
2023-03-04 12:59:59 +0530

    Make the context menu option generate the selection object

--- extension/index.js
+++ extension/index.js
@@ -1,18 +1,40 @@
-async function test() {
-  const currentWindow = browser.windows.getCurrent();
-}
-
 /**
  * @type typeof chrome
  */
 let ext_api = browser;
+const ID_ADD_SELECTED_TEXT_TO_COLLECTION = "add_selected_text_to_collection";
+
+async function getCurrentSelection() {
+    const [currentTab] = await ext_api.tabs.query({active: true, currentWindow: true});
+    let [{ result }] = await ext_api.scripting.executeScript({
+        target: { tabId: currentTab.id },
+        func: () => window.getSelection().toString(),
+    });
+    return result
+}
+
+async function getSnipObjectFromCurrentSelection() {
+    const [currentTab] = await ext_api.tabs.query({active: true, currentWindow: true});
+    let currentSelection = await getCurrentSelection();
+
+    return {
+        "source": currentTab.url,
+        "title": currentTab.title,
+        "content": currentSelection,
+        "date": (new Date()).toString()
+    };
+}
 
 ext_api.contextMenus.create({
   title: "Add Selected Text to Collection",
   contexts: ["selection"],
-  id: "txt2col_select",
+  id: ID_ADD_SELECTED_TEXT_TO_COLLECTION,
 });
 
 ext_api.contextMenus.onClicked.addListener((info, tab) => {
-  console.log(info.menuItemId);
+  if (info.menuItemId === ID_ADD_SELECTED_TEXT_TO_COLLECTION) {
+      getSnipObjectFromCurrentSelection().then(e => {
+          console.log(e);
+      });
+  }
 });
