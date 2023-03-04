741959be172a8e73b5aef7af262a2838f603f574
U-C-S <uchanakyasrinivas@gmail.com>
2023-03-04 14:09:04 +0530

    Migrate to Manifest 3

--- extension/index.js
+++ extension/index.js
@@ -1,28 +1,34 @@
 /**
  * @type typeof chrome
  */
-let ext_api = browser;
+let ext_api = chrome;
 const ID_ADD_SELECTED_TEXT_TO_COLLECTION = "add_selected_text_to_collection";
 
 async function getCurrentSelection() {
-    const [currentTab] = await ext_api.tabs.query({active: true, currentWindow: true});
-    let [{ result }] = await ext_api.scripting.executeScript({
-        target: { tabId: currentTab.id },
-        func: () => window.getSelection().toString(),
-    });
-    return result
+  const [currentTab] = await ext_api.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
+  let [{ result }] = await ext_api.scripting.executeScript({
+    target: { tabId: currentTab.id },
+    func: () => window.getSelection().toString(),
+  });
+  return result;
 }
 
 async function getSnipObjectFromCurrentSelection() {
-    const [currentTab] = await ext_api.tabs.query({active: true, currentWindow: true});
-    let currentSelection = await getCurrentSelection();
+  const [currentTab] = await ext_api.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
+  let currentSelection = await getCurrentSelection();
 
-    return {
-        "source": currentTab.url,
-        "title": currentTab.title,
-        "content": currentSelection,
-        "date": (new Date()).toString()
-    };
+  return {
+    source: currentTab.url,
+    title: currentTab.title,
+    content: currentSelection,
+    date: new Date().toString(),
+  };
 }
 
 ext_api.contextMenus.create({
@@ -33,8 +39,8 @@ ext_api.contextMenus.create({
 
 ext_api.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === ID_ADD_SELECTED_TEXT_TO_COLLECTION) {
-      getSnipObjectFromCurrentSelection().then(e => {
-          console.log(e);
-      });
+    getSnipObjectFromCurrentSelection().then((e) => {
+      console.log(e);
+    });
   }
 });
