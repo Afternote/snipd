ecaa14ae05fcd1e411b578f0fdde077caf905727
U-C-S <uchanakyasrinivas@gmail.com>
2023-03-04 12:36:42 +0530

    init context menu opts

--- extension/index.js
+++ extension/index.js
@@ -1,3 +1,18 @@
 async function test() {
-    const currentWindow = browser.windows.getCurrent()
+  const currentWindow = browser.windows.getCurrent();
 }
+
+/**
+ * @type typeof chrome
+ */
+let ext_api = browser;
+
+ext_api.contextMenus.create({
+  title: "Add Selected Text to Collection",
+  contexts: ["selection"],
+  id: "txt2col_select",
+});
+
+ext_api.contextMenus.onClicked.addListener((info, tab) => {
+  console.log(info.menuItemId);
+});
