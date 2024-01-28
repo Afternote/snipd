792cfac92792be2547abeb8859a722a8c25453df
Aditya Gupta <rockonman3@gmail.com>
2024-01-28 19:38:41 +0530

    makes the navbar scrollabe

--- notes_page/src/components/NavBar.jsx
+++ notes_page/src/components/NavBar.jsx
@@ -1,42 +1,41 @@
-import reactLogo from "../assets/snipdLogo.jpg";
-import { Stack, Divider, Title, Center, NavLink, Navbar, Button } from "@mantine/core";
+import React from "react";
+import { Stack, Divider, Title, Center, Navbar, Button } from "@mantine/core";
 import "../assets/print.css";
+import reactLogo from "../assets/snipdLogo.jpg";
 
-export default function ({ categoryList, setSelectedCategory }) {
+const CustomNavbar = ({ categoryList, setSelectedCategory }) => {
   return (
-    <Navbar className="nav-bar" width={{ base: 280 }} fixed p={10} style={{ backgroundColor: "white" }}>
+    <Navbar
+      className="nav-bar"
+      width={{ base: 280 }}
+      fixed
+      p={10}
+      style={{ backgroundColor: "white", maxHeight: "100vh", overflowY: "auto" }}
+    >
       <Center>
-        <img src={reactLogo} width="100px" />
+        <img src={reactLogo} width="100px" alt="React Logo" />
       </Center>
 
-      <Title order={2} align="left" color={"green"} mt={10}>
+      <Title order={2} align="left" color="green" mt={10}>
         Collections
       </Title>
       <Divider mb={5} mt={5} />
-      <Stack spacing={"xs"} color={"black"}>
-        {categoryList.map((category) => {
-          return (
-            <Button
-              variant="subtle"
-              color="dark"
-              onClick={() => {
-                setSelectedCategory(category);
-              }}>
-              {category}
-            </Button>
-          );
-        })}
+      <Stack spacing="xs" color="black">
+        {categoryList.map((category) => (
+          <Button
+            key={category}
+            variant="subtle"
+            color="dark"
+            onClick={() => {
+              setSelectedCategory(category);
+            }}
+          >
+            {category}
+          </Button>
+        ))}
       </Stack>
     </Navbar>
   );
-}
+};
 
-//        <Button variant="subtle" color="dark">
-//          History Research
-//        </Button>
-//        <Button variant="subtle" color="dark">
-//          Interesting Reddit Collection
-//        </Button>
-//        <Button variant="subtle" color="dark">
-//          Cats
-//        </Button>
+export default CustomNavbar;
