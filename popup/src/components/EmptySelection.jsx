eb28b23687f5e31095d386b190b538355177d5ef
Chanakya <uchanakyasrinivas@gmail.com>
2024-02-17 20:34:38 +0530

    Remove MUI dep from EmptySelection.jsx

--- popup/src/components/EmptySelection.jsx
+++ popup/src/components/EmptySelection.jsx
@@ -1,38 +1,34 @@
 import React, { useState } from "react";
-import { Card, CardActions, Typography } from "@mui/material";
-import astronautUfo from "../assets/ufo_ast.png";
-import { Button } from "@mantine/core";
-import { openAllSnipdPage } from "../utils/snippitUtils";
-import "../styles/EmptySelection.css";
+import { Button, Group, Image, Stack, Text } from "@mantine/core";
+
 import { NewNote } from "./NewNote";
+import { openAllSnipdPage } from "../utils/snippitUtils";
+import astronautUfo from "../assets/ufo_ast.png";
 
 export function EmptySelection() {
   const [isNewNote, setIsNewNote] = useState(false);
 
   const NoSelect = () => {
     return (
-      <div className="emptySelectionDiv">
+      <Stack h={"95vh"} align="center" justify="center">
         <center>
-          <img className="astronautUfoImage" src={astronautUfo} alt="Logo" />
-          <Typography className="margin16" variant="h6">
-            <b>
-              To save a snippet, please make a selection. <hr />
-              It appears that no text/image/link has been selected.
-            </b>
-            <br />
-          </Typography>
+          <Image w={"100%"} src={astronautUfo} alt="Logo" />
+          <Text m={16} fw={700} fz={18}>
+            To save a snippet, please make a selection. <hr />
+            It appears that no text/image/link has been selected.
+          </Text>
         </center>
 
-        <CardActions className="cardActions">
-          <Button className="margin16" onClick={() => setIsNewNote(true)}>
+        <Group position="center">
+          <Button m={16} onClick={() => setIsNewNote(true)}>
             Make note
           </Button>
 
-          <Button className="margin16" onClick={() => openAllSnipdPage()}>
+          <Button m={16} onClick={() => openAllSnipdPage()}>
             Central Page
           </Button>
-        </CardActions>
-      </div>
+        </Group>
+      </Stack>
     );
   };
 
