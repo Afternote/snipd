c4507db5d575ae667a4a200ea9c883b43961c439
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2024-02-20 21:29:21 +0530

    fixes the back button on notes page issue

--- popup/src/components/NewNote.jsx
+++ popup/src/components/NewNote.jsx
@@ -1,12 +1,11 @@
-// NewNote.js
-
 import React, { useState } from "react";
 import { CardActions, TextField } from "@mui/material";
 import { Button } from "@mantine/core";
 import { saveNote } from "../utils/snippitUtils";
 import "../styles/EmptySelection.css";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
-export function NewNote({ onClose }) {
+export function NewNote({ onClose, isNewNote }) {
   const [noteContent, setNoteContent] = useState("");
 
   const handleMakeNote = () => {
@@ -15,8 +14,13 @@ export function NewNote({ onClose }) {
     });
   };
 
+  const handleBackClick = () => {
+    onClose()
+  }
+
   return (
     <>
+    <ArrowBackIcon style={{ paddingTop:"8px", paddingBottom:"8px", height:"16px", width:"16px"}} onClick={handleBackClick}/>
       <TextField
         label="Note content"
         variant="outlined"
