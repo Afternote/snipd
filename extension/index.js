80217fa75ed59bddfd6ae2980bf2bd5154d907f2
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2024-03-02 00:38:29 +0530

    minor fix

--- extension/index.js
+++ extension/index.js
@@ -1,71 +1,88 @@
+/**
+ * @type typeof chrome
+ */
+let ext_api = chrome;
 const ID_ADD_SELECTED_TEXT_TO_COLLECTION = "add_selected_text_to_collection";
 
-const extApi = chrome;
-
-
-async function setSidePanelOptions(enabled) {
-  await extApi.sidePanel.setOptions({ enabled });
-}
-
-function setPanelBehavior() {
-  extApi.sidePanel.setPanelBehavior({ openPanelOnActionClick: true })
-    .catch((error) => console.error(error));
-}
-
-function createContextMenu(title, contexts, id) {
-  extApi.contextMenus.create({ title, contexts, id });
-}
-
-function setStorageDefaults() {
-  extApi.storage.local.get(["snipd_store"]).then((e) => {
-    if (!e.snipd_store) extApi.storage.local.set({ snipd_store: [] });
+chrome.tabs.onActivated.addListener(async function(activeInfo) {
+  await chrome.sidePanel.setOptions({
+    enabled: false
   });
-
-  extApi.storage.local.get(["snipd_categories"]).then((e) => {
-    if (!e.snipd_categories) extApi.storage.local.set({ snipd_categories: ["Default"] });
-  });
-}
-
-
-
-chrome.tabs.onActivated.addListener(async function (activeInfo) {
-  await setSidePanelOptions(false);
 });
 
-chrome.action.onClicked.addListener(function () {
-  setSidePanelOptions(true);
+chrome.action.onClicked.addListener(function(){
+  chrome.sidePanel.setOptions({
+    enabled: true
+  })
+
+}
+)
+chrome.sidePanel
+  .setPanelBehavior({  
+    
+    openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
+
+ext_api.contextMenus.create({
+  title: "Add Selected Text to Collection",
+  contexts: ["selection"],
+  id: ID_ADD_SELECTED_TEXT_TO_COLLECTION,
 });
 
-setPanelBehavior();
+//context menu item for image
+ext_api.contextMenus.create({
+  title: "Add Image to Collection",
+  contexts: ["image"],
+  id: "add_image_to_collection",
+});
 
-createContextMenu("Add Selected Text to Collection", ["selection"], ID_ADD_SELECTED_TEXT_TO_COLLECTION);
-createContextMenu("Add Image to Collection", ["image"], "add_image_to_collection");
-createContextMenu("Add Link to Collection", ["page"], "add_link_to_collection");
+ext_api.contextMenus.create({
+  title: "Add Link to Collection",
+  contexts: ["page"],
+  id: "add_link_to_collection",
+});
 
-extApi.contextMenus.onClicked.addListener((info, tab) => {
-  setSidePanelOptions(true);
-
-  function handleMenuItem(menuItemId, snipType, snipContent) {
-    extApi.storage.local.set({ snip_type: snipType, snip_content: snipContent });
-    extApi.sidePanel.open({ windowId: tab.windowId });
-  }
-
+ext_api.contextMenus.onClicked.addListener((info, tab) => {
+  chrome.sidePanel.setOptions({
+    enabled: true
+  });
   switch (info.menuItemId) {
     case "add_image_to_collection":
-      handleMenuItem("image", info.srcUrl);
+      chrome.storage.local.set({
+        snip_type: "image",
+        snip_content: info.srcUrl,
+      });
+      ext_api.sidePanel.open({ windowId: tab.windowId });
       break;
 
-    case ID_ADD_SELECTED_TEXT_TO_COLLECTION:
+    case "add_selected_text_to_collection":
       chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        handleMenuItem("text", info.selectionText);
+          chrome.storage.local.set({
+            snip_type: "text",
+            snip_content: info.selectionText,
+          });
+
+          chrome.sidePanel.open({ windowId: tabs[0].windowId });
+        
       });
       break;
 
     case "add_link_to_collection":
-      handleMenuItem("link", info.pageUrl);
+      chrome.storage.local.set({
+        snip_type: "link",
+        snip_content: info.pageUrl,
+      });
+      ext_api.sidePanel.open({ windowId: tab.windowId });
       break;
   }
 });
 
+// Make a storage array if it doesn't exist
+ext_api.storage.local.get(["snipd_store"]).then((e) => {
+  if (!e.snipd_store) ext_api.storage.local.set({ snipd_store: [] });
+});
 
-setStorageDefaults();
+// Make a storage array if it doesn't exist
+ext_api.storage.local.get(["snipd_categories"]).then((e) => {
+  if (!e.snipd_categories) ext_api.storage.local.set({ snipd_categories: ["Default"] });
+});
--- popup/src/components/CategoriesMenuMantine.jsx
+++ popup/src/components/CategoriesMenuMantine.jsx
@@ -1,36 +1,28 @@
-import React from "react";
-import { Menu, Button, Text, rem, MantineProvider, ScrollArea } from "@mantine/core";
+import React, { useRef, useState, useEffect } from "react";
+import { Menu, Button, Text, MantineProvider, ScrollArea } from "@mantine/core";
 import NewCategory from "./NewCategory.";
 import { IconCategoryPlus, IconCategory2 } from "@tabler/icons-react";
 
 const CategoriesMenuMantine = ({ addCategory, categoriesList, setCategory }) => {
-  const [custom, setCustom] = React.useState(false);
-  const [categoryAdded, setCategoryAdded] = React.useState('')
+  const [custom, setCustom] = useState(false);
+  const [categoryAdded, setCategoryAdded] = useState('');
 
-  const viewport = React.useRef(null);
-  React.useEffect(() => {
+  const viewport = useRef(null);
+
+  useEffect(() => {
     viewport.current?.scrollTo({ top: viewport.current?.scrollHeight, behavior: 'smooth' });
-
-  }, [categoryAdded] )
+  }, [categoryAdded]);
 
   const handleCustomClose = () => {
     setCustom(true);
   };
 
-  const handleClose = (event) => {
-    setAnchorEl(null);
-  };
   return (
-    <Menu shadow="md" width={200} >
+    <Menu shadow="md" width={200}>
       <Menu.Target>
         <Button style={{ width: "60%", marginBottom: "24px", marginTop: "8px" }}>
-          <MantineProvider
-            theme={{
-              fontFamily: "Roboto",
-            }}>
-            <Text fz="md" lh="sm" style={{ padding: "8px" }}>
-              Categories
-            </Text>
+          <MantineProvider theme={{ fontFamily: "Roboto" }}>
+            <Text fz="md" lh="sm" style={{ padding: "8px" }}>Categories</Text>
           </MantineProvider>
         </Button>
       </Menu.Target>
@@ -38,19 +30,18 @@ const CategoriesMenuMantine = ({ addCategory, categoriesList, setCategory }) => 
       <Menu.Dropdown onKeyDown={(e) => e.stopPropagation()} onClick={(e) => e.stopPropagation()}>
         <Menu.Label>Categories</Menu.Label>
         <ScrollArea style={{ height: 250 }} viewportRef={viewport}>
-          {categoriesList.map((category) => {
-            return (
-              <Menu.Item
-                rightSection={<IconCategory2 style={{ width: rem(14), height: rem(14) }} />}
-                key={category}
-                onClick={(e) => {
-                    e.stopPropagation(); // Stop the event from bubbling up
-                  setCategory(category);
-                }}>
-                {category}
-              </Menu.Item>
-            );
-          })}
+          {categoriesList.map((category) => (
+            <Menu.Item
+              rightSection={<IconCategory2 style={{ width: "1rem", height: "1rem" }} />}
+              key={category}
+              onClick={(e) => {
+                e.stopPropagation();
+                setCategory(category);
+              }}
+            >
+              {category}
+            </Menu.Item>
+          ))}
         </ScrollArea>
         <Menu.Divider />
 
@@ -58,17 +49,13 @@ const CategoriesMenuMantine = ({ addCategory, categoriesList, setCategory }) => 
         {!custom ? (
           <Button
             variant="outline"
-            onClick={(e) => {
-              e.stopPropagation(); 
-              handleCustomClose();
-            }}
-            onKeyDown={(e) => e.stopPropagation()}
-            >
+            onClick={(e) => { e.stopPropagation(); handleCustomClose(); }}
+          >
             Add a new Category
-            <IconCategoryPlus style={{paddingLeft:'16px'}}size={14} />
+            <IconCategoryPlus style={{ paddingLeft: "16px" }} size={14} />
           </Button>
         ) : (
-            <NewCategory setCustom={setCustom} addCategory={addCategory} setCategoryAdded={setCategoryAdded} style={{ margin: "8px" }} />
+          <NewCategory setCustom={setCustom} addCategory={addCategory} setCategoryAdded={setCategoryAdded} style={{ margin: "8px" }} />
         )}
       </Menu.Dropdown>
     </Menu>
