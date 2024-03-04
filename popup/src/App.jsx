c13288f4fa41bfa47161b5fc43acae7df285c089
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2024-03-04 14:55:00 +0530

    fixes snipd selection bug

--- popup/src/App.jsx
+++ popup/src/App.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import Note from "./components/Note";
 import { EmptySelection } from "./components/EmptySelection";
 
+let disableListener = false; // Outside useEffect
+
 async function getCurrentSelectionData() {
   const [currentTab] = await chrome.tabs.query({
     active: true,
@@ -11,7 +13,9 @@ async function getCurrentSelectionData() {
     target: { tabId: currentTab.id },
     func: async () => {
       let selectionText = await chrome.storage.local.get(["snip_content", "snip_type"]);
-
+      disableListener = true; // Temporarily disable
+      await chrome.storage.local.remove(["snip_content", "snip_type"]);
+      disableListener = false; // Re-enable
       if (!window.location.href.endsWith(".pdf") && selectionText.snip_content === undefined) {
         return {
           snip_content: window.getSelection().toString(),
@@ -28,42 +32,49 @@ async function getCurrentSelectionData() {
     title: currentTab.title,
     type: result.snip_type,
     content: result.snip_content,
-    date: new Date().toLocaleDateString() + ", " + new Date().toLocaleTimeString() ,
+    date: new Date().toLocaleDateString() + ", " + new Date().toLocaleTimeString(),
   };
 }
 
 function App() {
   const [snipd, setSnipd] = useState();
 
-  
-
   useEffect(() => {
     const updateSnipdFromSelection = () => {
-        getCurrentSelectionData()
-            .then((selection) => {
-                setSnipd(selection);
-                console.log(selection);
-            });
+      getCurrentSelectionData().then((selection) => {
+        setSnipd(selection);
+        console.log(selection);
+      });
     };
 
-    updateSnipdFromSelection(); 
+    updateSnipdFromSelection();
+
+    // const listener = (changes, namespace) => {
+    //     if (namespace === 'local') {
+    //         updateSnipdFromSelection();
+    //     }
+    // };
+
+    // const listener = (changes, namespace) => {
+    //   if (namespace === "local" && (changes.snip_content || changes.snip_type)) {
+    //     updateSnipdFromSelection();
+    //   }
+    // };
 
     const listener = (changes, namespace) => {
-        if (namespace === 'local') {
-            updateSnipdFromSelection();
-        }
-    };
+      if (namespace === 'local' && !disableListener) { 
+          updateSnipdFromSelection();
+      }
+  };
 
     chrome.storage.onChanged.addListener(listener);
     return () => {
-        chrome.storage.onChanged.removeListener(listener); 
+      chrome.storage.onChanged.removeListener(listener);
     };
-}, []); 
+  }, []);
 
   return (
-    <div
-      className="App"
-      >
+    <div className="App">
       {!!snipd?.content ? (
         <Note
           snipd={snipd}
