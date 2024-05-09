3cf291dd79bfeda212628f16927e8ce0305596dc
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2024-05-09 20:56:53 +0530

    variables refactored

--- extension/src/data/variables.js
+++ extension/src/data/variables.js
@@ -1,31 +1,12 @@
-/// Currently non user-configurable settings 
-var convertWhenOnlyFewWordsSelected = true;
-var wordsLimitToProccessText = 5;
 var secondaryColor = 'lightBlue';
-var linkSymbolsToShow = 20;
 var selectionHandleLineHeight = 25;
-
-var copyLabel = 'Copy';
 var addToCollectionLabel = 'Add to collection +';
-var openLinkLabel = 'Open';
-var translateLabel = 'Translate';
-var showOnMapLabel = 'Show on map';
-var cutLabel = 'Cut';
-var pasteLabel = 'Paste';
-var dictionaryLabel = 'Dictionary';
-var markerLabel = 'Highlight';
-var italicLabel = 'Italic';
-var boldLabel = 'Bold';
-var strikeLabel = 'Strike';
-var clearLabel = 'Clear';
 
-/// Dynammically assigned variables
 var selection, selectedText;
-var tooltip, secondaryTooltip, arrow, infoPanel, searchButton, copyButton, verticalSecondaryTooltip;
+var tooltip, secondaryTooltip, arrow, infoPanel, searchButton, verticalSecondaryTooltip;
 var tooltipIsShown = false, dontShowTooltip = false, isDraggingTooltip = false, isDraggingDragHandle = false, isDarkTooltip = true;
 var draggingHandleIndex, lastMouseUpEvent, ratesLastFetchedDate;
 var firstButtonBorderRadius = '3px 0px 0px 3px', lastButtonBorderRadius = '0px 3px 3px 0px', onlyButtonBorderRadius = '3px';
-var browserLanguage, browserCurrency, browserMetricSystem;
 var tooltipOnBottom = false, configsWereLoaded = false, currencyRatesWereLoaded = false, isTextFieldFocused = false;
 var isTextFieldEmpty = true, domainIsBlacklistedForSnapping, selectedTextIsCode, addButtonIcons;
 var timerToRecreateOverlays, delayToRecreateOverlays = 150;
