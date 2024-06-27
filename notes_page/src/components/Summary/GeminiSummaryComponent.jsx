fee56840ad08f7a05aa26f95bbc5fa28e78e917e
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2024-06-27 18:25:28 +0530

    fixes summary error on empty list

--- notes_page/src/components/Summary/GeminiSummaryComponent.jsx
+++ notes_page/src/components/Summary/GeminiSummaryComponent.jsx
@@ -1,6 +1,18 @@
-import React from "react";
-import { Skeleton, Text } from "@mantine/core";
+import React, { useState } from "react";
+import { Skeleton, Text, Button, ActionIcon } from "@mantine/core";
+
 const GeminiSummaryComponent = (props) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopyClick = async () => {
+    try {
+      await navigator.clipboard.writeText(props.summary);
+      setCopied(true);
+    } catch (error) {
+      console.error("Failed to copy text: ", error);
+    }
+  };
+
   return (
     <div>
       {props.loading && (
@@ -17,6 +29,15 @@ const GeminiSummaryComponent = (props) => {
       {!props.loading && (
         <div>
           <Text>{props.summary}</Text>
+          <div style={{display:'flex', justifyContent:'right'}}>
+            <Button
+              variant="light"
+              style={{ margin: "16px 0px 16px 16px" }}
+              onClick={handleCopyClick}
+            >
+              {copied ? "Copied!" : "Copy"}
+            </Button>
+          </div>
         </div>
       )}
     </div>
--- notes_page/src/components/Summary/SummarizeSnippetsModalComponent.jsx
+++ notes_page/src/components/Summary/SummarizeSnippetsModalComponent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Modal, Button } from "@mantine/core";
-import { useState, useEffect } from "react"; 
-import { NativeSelect } from "@mantine/core";
+import { useState, useEffect } from "react";
+import { NativeSelect, Text } from "@mantine/core";
 import IconGemini from "../../assets/geminiIcon.png";
 import { fetchDataFromChromeStorageByCategory } from "../../utils/snipUtils";
 import CACHE_KEYS from "../../utils/cacheKeys";
@@ -12,6 +12,7 @@ const SummarizeSnippetsModalComponent = (props) => {
   const [loading, setLoading] = useState(false);
   const [summary, setSummary] = useState("");
   const [summarizing, setSummarizing] = useState(false);
+  const [showError, setShowError] = useState(false);
 
   useEffect(() => {
     return () => {
@@ -21,51 +22,80 @@ const SummarizeSnippetsModalComponent = (props) => {
     };
   }, []);
 
+  useEffect(() => {
+    let timeoutId;
+
+    if (showError) {
+      timeoutId = setTimeout(() => {
+        setShowError(false);
+      }, 5000); // 5000 milliseconds = 5 seconds
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [showError]);
+
   /**
    * Handles the click event for the "Summarize" button.
    * Sends a POST request to the Gemini API with the selected category's data.
    * Updates the summary state with the response from the API.
-   * @returns {Promise<void>} 
+   * @returns {Promise<void>}
    */
   const handleSummarizeClick = async () => {
     try {
       // Reset the summary and loading states
-      setSummarizing(true);
-      setSummary(""); 
+      setSummary("");
       setLoading(true);
 
       // Fetch data from Chrome Storage by category
-      const data= await fetchDataFromChromeStorageByCategory(CACHE_KEYS.SNIPD_STORE, value);
-      const dataWithoutImagesOrLinks = data.filter((item) => item.type !== "image" && item.type !== "link");
-      // Send a POST request to the Gemini API
-      const response = await fetch(
-        "http://ec2-13-200-252-204.ap-south-1.compute.amazonaws.com:4040/gemini",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(dataWithoutImagesOrLinks),
-        }
+      const data = await fetchDataFromChromeStorageByCategory(CACHE_KEYS.SNIPD_STORE, value);
+      const dataWithoutImagesOrLinks = data.filter(
+        (item) => item.type !== "image" && item.type !== "link"
       );
 
-      // Check if the response was successful
-      if (!response.ok) {
-        throw new Error("Failed to fetch data from the API");
-      }
+      if (dataWithoutImagesOrLinks.length === 0) {
+        setShowError(true);
+        throw new Error("No data to summarize");
+      } else {
+        setSummarizing(true);
+        // Send a POST request to the Gemini API
+        const response = await fetch(
+          "http://ec2-13-200-252-204.ap-south-1.compute.amazonaws.com:4040/gemini",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(dataWithoutImagesOrLinks),
+          }
+        );
 
-      // Parse the response JSON
-      const responseJson = await response.text();
-      if (!responseJson) {
-        throw new Error("Empty response from the API");
-      }
-      const apiResponse = JSON.parse(responseJson);
-      if (!apiResponse || !apiResponse.Candidates || !apiResponse.Candidates[0] || !apiResponse.Candidates[0].Content || !apiResponse.Candidates[0].Content.Parts || !apiResponse.Candidates[0].Content.Parts[0]) {
-        throw new Error("Invalid response format from the API");
-      }
+        // Check if the response was successful
+        if (!response.ok) {
+          throw new Error("Failed to fetch data from the API");
+        }
 
-      // Update the summary state with the response content
-      setSummary(apiResponse.Candidates[0].Content.Parts[0]);
+        // Parse the response JSON
+        const responseJson = await response.text();
+        if (!responseJson) {
+          throw new Error("Empty response from the API");
+        }
+        const apiResponse = JSON.parse(responseJson);
+        if (
+          !apiResponse ||
+          !apiResponse.Candidates ||
+          !apiResponse.Candidates[0] ||
+          !apiResponse.Candidates[0].Content ||
+          !apiResponse.Candidates[0].Content.Parts ||
+          !apiResponse.Candidates[0].Content.Parts[0]
+        ) {
+          throw new Error("Invalid response format from the API");
+        }
+
+        // Update the summary state with the response content
+        setSummary(apiResponse.Candidates[0].Content.Parts[0]);
+      }
     } catch (error) {
       // Log any errors that occur
       console.error("Error:", error);
@@ -89,6 +119,11 @@ const SummarizeSnippetsModalComponent = (props) => {
         centered>
         {!summarizing && (
           <div style={{ display: "flex", flexDirection: "column" }}>
+            {showError && (
+              <div>
+                <Text size="sm" c="red.4">404, the selected category has no text snippets</Text>
+              </div>
+            )}
             <NativeSelect
               size="xs"
               label="Select a Category"
